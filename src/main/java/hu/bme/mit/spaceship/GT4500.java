c9c5b84eeb070c27f9967ff0e24cbb9b90dcd0a1
meres <moczardavid@gmail.com>
2017-04-06 23:55:37 -0700

    case statement changed to if

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -38,8 +38,7 @@ public class GT4500 implements SpaceShip {
 
     boolean firingSuccess = false;
 
-    switch (firingMode) {
-      case SINGLE:
+    if (firingMode==SINGLE) {
         if (wasPrimaryFiredLast) {
           // try to fire the secondary first
           if (! secondaryTorpedoStore.isEmpty()) {
@@ -75,8 +74,8 @@ public class GT4500 implements SpaceShip {
           }
         }
         break;
-
-      case ALL:
+      }
+      if(firingMode == ALL) {
         // try to fire both of the torpedos
        if(!primaryTorpedoStore.isEmpty() && !secondaryTorpedoStore.isEmpty()){
 		firingSuccess = primaryTorpedoStore.fire(1);
@@ -84,7 +83,7 @@ public class GT4500 implements SpaceShip {
 		wasPrimaryFiredLast = false;
 	}
         break;
-    }
+       }
 
     return firingSuccess;
   }
